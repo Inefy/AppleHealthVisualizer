62330a6f7dc4b21df11e54f547bfb7745c3df8a2
Inefy <zbatten@mun.ca>
2023-04-24 20:25:26 -0230

    correctly displays number of hours slept

--- sleep_analysis.py
+++ sleep_analysis.py
@@ -17,6 +17,7 @@ def get_sleep_records_from_xml(xml_file):
 def preprocess_sleep_data(data):
     data['type'] = data['type'].astype(str)
     sleep_data = data[data['type'] == 'HKCategoryTypeIdentifierSleepAnalysis']
+    sleep_data = sleep_data[sleep_data['value'] == 'HKCategoryValueSleepAnalysisAsleepUnspecified']
     sleep_data['startDate'] = pd.to_datetime(sleep_data['startDate'])
     sleep_data['endDate'] = pd.to_datetime(sleep_data['endDate'])
     return sleep_data
@@ -27,7 +28,10 @@ def analyze_sleep_data(sleep_data):
     return daily_sleep
 
 def visualize_sleep_data(daily_sleep):
+    # Convert sleep duration to hours and minutes
+    daily_sleep = daily_sleep.dt.total_seconds() / 3600
     daily_sleep.plot(kind='bar', figsize=(15, 5), title='Daily Sleep Duration')
     plt.xlabel('Date')
-    plt.ylabel('Sleep Duration (seconds)')
+    plt.ylabel('Sleep Duration (hours)')
     plt.show()
+
