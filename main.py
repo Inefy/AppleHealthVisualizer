61b7cfa8b43f8856be7d5c4a096f1949bb45e519
Inefy <zbatten@mun.ca>
2023-04-24 13:31:12 -0230

    working prototype

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-from sleep_analysis import read_data, preprocess_sleep_data, analyze_sleep_data, visualize_sleep_data
+from sleep_analysis import get_sleep_records_from_xml, preprocess_sleep_data, analyze_sleep_data, visualize_sleep_data
 from heart_rate_analysis import preprocess_heart_rate_data, analyze_heart_rate_data, visualize_heart_rate_data
 from steps_analysis import preprocess_steps_data, analyze_steps_data, visualize_steps_data
 
+def get_data_range(data, start_date, end_date):
+    return data[(data['startDate'] >= start_date) & (data['endDate'] <= end_date)]
+
 if __name__ == "__main__":
     file_path = "/home/zacbatten/Documents/projects/appleHealth/apple_health_export/export.xml"
-    raw_data = read_data(file_path)
+    raw_data = get_sleep_records_from_xml(file_path)
 
-    sleep_data = preprocess_sleep_data(raw_data)
-    daily_sleep = analyze_sleep_data(sleep_data)
-    visualize_sleep_data(daily_sleep)
+    data_type = input("Enter the data type you want to analyze (heart_rate, steps, sleep): ")
+    start_date = input("Enter the start date for the data range (yyyy-mm-dd): ")
+    end_date = input("Enter the end date for the data range (yyyy-mm-dd): ")
 
-    heart_rate_data = preprocess_heart_rate_data(raw_data)
-    daily_heart_rate = analyze_heart_rate_data(heart_rate_data)
-    visualize_heart_rate_data(daily_heart_rate)
+    if data_type.lower() == "sleep":
+        sleep_data = preprocess_sleep_data(raw_data)
+        sleep_data = get_data_range(sleep_data, start_date, end_date)
+        daily_sleep = analyze_sleep_data(sleep_data)
+        visualize_sleep_data(daily_sleep)
 
-    steps_data = preprocess_steps_data(raw_data)
-    daily_steps = analyze_steps_data(steps_data)
-    visualize_steps_data(daily_steps)
+    elif data_type.lower() == "heart_rate":
+        heart_rate_data = preprocess_heart_rate_data(raw_data)
+        heart_rate_data = get_data_range(heart_rate_data, start_date, end_date)
+        daily_heart_rate = analyze_heart_rate_data(heart_rate_data)
+        visualize_heart_rate_data(daily_heart_rate)
+
+    elif data_type.lower() == "steps":
+        steps_data = preprocess_steps_data(raw_data)
+        steps_data = get_data_range(steps_data, start_date, end_date)
+        daily_steps = analyze_steps_data(steps_data)
+        visualize_steps_data(daily_steps)
+
+    else:
+        print("Invalid data type entered.")
--- sleep_analysis.py
+++ sleep_analysis.py
@@ -15,20 +15,22 @@ def read_data(file_path):
     data = pd.DataFrame(records)
     return data
 
-def preprocess_sleep_data(data):
+def preprocess_sleep_data(data, start_date, end_date):
     data['type'] = data['type'].astype(str)
-    sleep_data = data[data['type'] == 'HKCategoryTypeIdentifierSleepAnalysis']
-    sleep_data['startDate'] = pd.to_datetime(sleep_data['startDate'])
-    sleep_data['endDate'] = pd.to_datetime(sleep_data['endDate'])
+    sleep_data = data[data['type'] == 'HKCategoryTypeIdentifierSleepAnalysis'].copy()
+    sleep_data.loc[:, 'startDate'] = pd.to_datetime(sleep_data['startDate'])
+    sleep_data.loc[:, 'endDate'] = pd.to_datetime(sleep_data['endDate'])
+    sleep_data = sleep_data[(sleep_data['startDate'] >= start_date) & (sleep_data['endDate'] <= end_date)]
     return sleep_data
 
 def analyze_sleep_data(sleep_data):
     sleep_data['duration'] = sleep_data['endDate'] - sleep_data['startDate']
     daily_sleep = sleep_data.groupby(sleep_data['startDate'].dt.date)['duration'].sum()
+    daily_sleep = daily_sleep.dt.total_seconds() / 3600  # Convert duration to hours
     return daily_sleep
 
 def visualize_sleep_data(daily_sleep):
     daily_sleep.plot(kind='bar', figsize=(15, 5), title='Daily Sleep Duration')
     plt.xlabel('Date')
-    plt.ylabel('Sleep Duration (seconds)')
+    plt.ylabel('Sleep Duration (hours)')
     plt.show()
