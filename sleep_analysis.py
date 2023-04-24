747fa7df15c5787aa483df43b131d9b27ba03c8b
Inefy <zbatten@mun.ca>
2023-04-24 13:44:47 -0230

    sleep analysis fixed

--- sleep_analysis.py
+++ sleep_analysis.py
@@ -1,36 +1,33 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from lxml import etree
+import xml.etree.ElementTree as ET
 
-def read_data(file_path):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        content = f.read()
-    root = etree.fromstring(content.encode('utf-8'))
+def get_sleep_records_from_xml(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
 
     records = []
+
     for record in root.findall('.//Record'):
-        attributes = record.attrib
-        records.append(attributes)
+        records.append(record.attrib)
 
     data = pd.DataFrame(records)
     return data
 
-def preprocess_sleep_data(data, start_date, end_date):
+def preprocess_sleep_data(data):
     data['type'] = data['type'].astype(str)
-    sleep_data = data[data['type'] == 'HKCategoryTypeIdentifierSleepAnalysis'].copy()
-    sleep_data.loc[:, 'startDate'] = pd.to_datetime(sleep_data['startDate'])
-    sleep_data.loc[:, 'endDate'] = pd.to_datetime(sleep_data['endDate'])
-    sleep_data = sleep_data[(sleep_data['startDate'] >= start_date) & (sleep_data['endDate'] <= end_date)]
+    sleep_data = data[data['type'] == 'HKCategoryTypeIdentifierSleepAnalysis']
+    sleep_data['startDate'] = pd.to_datetime(sleep_data['startDate'])
+    sleep_data['endDate'] = pd.to_datetime(sleep_data['endDate'])
     return sleep_data
 
 def analyze_sleep_data(sleep_data):
     sleep_data['duration'] = sleep_data['endDate'] - sleep_data['startDate']
     daily_sleep = sleep_data.groupby(sleep_data['startDate'].dt.date)['duration'].sum()
-    daily_sleep = daily_sleep.dt.total_seconds() / 3600  # Convert duration to hours
     return daily_sleep
 
 def visualize_sleep_data(daily_sleep):
     daily_sleep.plot(kind='bar', figsize=(15, 5), title='Daily Sleep Duration')
     plt.xlabel('Date')
-    plt.ylabel('Sleep Duration (hours)')
+    plt.ylabel('Sleep Duration (seconds)')
     plt.show()
